5e74f4325fd19814f2ea9b312537f42f20c06db0
silviu-negoita <negoita.silviuionut@gmail.com>
2016-05-04 15:55:34 +0300

    commit for sdiff

---
+++ launcher-for-updateable-code/src/org/flowerplatform/launcher/Downloader.java
@@ -0,0 +1,16 @@
+package org.flowerplatform.launcher;
+
+import java.io.File;
+
+/**
+ * @author Silviu Negoita
+ */
+public class Downloader {
+	final static String ARCHIVE_DOWNLOAD_NAME = "download.zip";
+	public static void downloadAndUnzip(String url, File location) {
+		
+		//TODO download a zip from url to location
+		
+		//TOOD unzip arhive
+	}
+}
--- launcher-for-updateable-code/src/org/flowerplatform/launcher/LauncherForUpdateableCode.java
+++ launcher-for-updateable-code/src/org/flowerplatform/launcher/LauncherForUpdateableCode.java
@@ -1,5 +1,7 @@
 package org.flowerplatform.launcher;
 
+import java.io.File;
+
 public class LauncherForUpdateableCode {
 	String previousBinDir;
 	String currentBinDir;
@@ -13,12 +15,43 @@ public class LauncherForUpdateableCode {
 		this.className = className;
 	}
 	
-	public void launch(String param) throws ClassNotFoundException {
-		// TODO check if newBin exist. if true, delete prevBin, curBin = prevBin, newBin = curBin
-		
-		// TODO load all jar/zips from curBin. 
-		
-		// TODO init launcherClass and start
+	public void deleteFolder(File folder) {
+	    File[] files = folder.listFiles();
+	    if(files!=null) { //some JVMs return null for empty dirs
+	        for(File f: files) {
+	            if(f.isDirectory()) {
+	                deleteFolder(f);
+	            } else {
+	                f.delete();
+	            }
+	        }
+	    }
+	    folder.delete();
 	}
+	
+	public void launch(LauncherDto param) throws ClassNotFoundException {
+		//TODO check if newBin exist. if true, delete prevBin, curBin = prevBin, newBin = curBin
+		File newBin = new File(currentBinDir);
+		if (newBin.exists()) {
+			File prevBin = new File(previousBinDir);
+			deleteFolder(prevBin);
+			previousBinDir = currentBinDir;
+			currentBinDir = newBinDir;
+		}
+		
+		
+		try {
+			//TODO load all jar/zips from curBin. 
+			
+	        //TODO init launcherClass and start	        
+	      
+	        
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
 	
 }
